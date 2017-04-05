fc70aad56a2f841f4d64d48d9303f8238d6b1b42
Joseph Smith <joseph.smith@guardian.co.uk>
2017-04-05 11:56:19 +0100

    Add closure scope stuff

---
+++ week1/variable-scope/closure-advanced.js
@@ -0,0 +1,20 @@
+x = 3;
+
+function getObj() {
+	let x = 3;
+
+	return {
+		incrementX() { x++ },
+		getX() { return x }
+	}
+}
+
+const o = getObj();
+
+console.log(`initial inner x: ${o.getX()}`);
+
+o.incrementX();
+
+console.log(`incremented inner x: ${o.getX()}`);
+
+console.log(`global x: ${x}`);
