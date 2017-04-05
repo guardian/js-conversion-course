56a055b0ae930cf65484e7594b257d96b3c9a0f1
Sam Desborough <sam.desborough@gmail.com>
2017-04-05 12:18:35 +0100

    add commented/uncommented versions

--- week1/variable-scope/this.js
+++ week1/variable-scope/this.js
@@ -7,30 +7,18 @@ function g() {
   return f(100);
 }
 
-console.log(f()); // NaN, because on its own `f` has no `this.x`
-console.log(g()); // 500, because `f` inherts `g`'s context (accessible as `this`)
+console.log(f()); 
+console.log(g()); 
 
 
-const mult5 = f.bind({ x: 5 }); // `bind` creates a new function 
-                                // with the context provided as a parameter
+const mult5 = f.bind({ x: 5 }); 
 
-console.log(mult5(2)); // 10
+console.log(mult5(2));
 
+const h = f.bind({ x: 5 }, 10);
 
-const get50 = f.bind({ x: 5 }, 10); // you can also provide parameters when
-                                    // creating a function with `bind`
-console.log(get50()); 
+console.log(h()); 
 
+console.log(f.call({ x: 5 }, 3)); 
 
-console.log(f.call({ x: 5 }, 3)); // 15
-                                  // `call` also allows you to specify the
-                                  // context, but instead of creating a new
-                                  // function, you provide arguments from the
-                                  // 2nd parameter onwards
-
-                                  
-console.log(f.apply({ x: 5 }, [4])); // 20
-                                     // `apply` is similar to `call` but the 
-                                     // arguments are provided in an array 
-                                     // rather than as separate parameters  
-
+console.log(f.apply({ x: 5 }, [4]));
