b5ec1c2aeaa6b355c82017aa479264591b2c8c45
Simon Adcock <simon.adcock@guardian.co.uk>
2017-04-26 14:08:08 +0100

    commented object example code

--- week1/objects/with-comments/getters-and-setters.js
+++ week1/objects/with-comments/getters-and-setters.js
@@ -3,8 +3,22 @@ const obj = {
     b: 'world'
 };
 
+// There are two ways to directly access properties on an object
+
+// 1) dot notation (if property name is known)
 console.log('Dot notation: ' + obj.a);
+
+// 2) square bracket notation (to dynamically access property)
 console.log('Square bracket notation: ' + obj['a']);
+
+// Dynamic access example:
+function getProperty(name) {
+    return obj[name];
+}
+
+console.log('Dynamic access example: ' + getProperty('a'));
+
+// You can assign (and reassign) properties in the same way:
 
 obj.c = 'foo';
 
@@ -15,6 +29,9 @@ obj['c'] = 'bar';
 console.log('Reassignment: ' + obj.c);
 
 console.log('========');
+
+// Enumerating property names
+
 console.log('Enumerating keys:');
 
 const keys = Object.keys(obj);
@@ -24,6 +41,9 @@ for (let i = 0; i < keys.length; i++) {
 }
 
 console.log('========');
+
+// Looping over properties
+
 console.log('Looping over properties:');
 
 for (let prop in obj) {
